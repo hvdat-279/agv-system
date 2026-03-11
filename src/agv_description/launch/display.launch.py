949e1a55070afef11f5dc7383295f88e5ce2eebb
hvdat-279 <toiladat279@gmail.com>
2026-03-11 20:37:40 +0700

    build: init project agv system

---
+++ src/agv_description/launch/display.launch.py
@@ -0,0 +1,40 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from xacro import process_file
+
+def generate_launch_description():
+    # Lấy đường dẫn chính xác đến package
+    pkg_dir = get_package_share_directory('agv_description')
+    
+    # Đường dẫn đến file URDF
+    urdf_file = os.path.join(pkg_dir, 'urdf', 'agv.urdf.xacro')
+    
+    print("Đang đọc file URDF từ:", urdf_file)  # In ra để kiểm tra
+    
+    # Xử lý file xacro
+    doc = process_file(urdf_file)
+    robot_desc = doc.toxml()
+    
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_desc}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
